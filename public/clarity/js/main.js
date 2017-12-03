8fbea026179b7fab8bf8b7674ff831310aa86787
Gabriel <gabriel89oliveira@gmail.com>
2017-12-03 11:33:05 -0200

    Ajustes barra de carregar

--- public/clarity/js/main.js
+++ public/clarity/js/main.js
@@ -176,8 +176,8 @@
 	
 	//<![CDATA[
 		$(window).load(function() { // makes sure the whole site is loaded
-			$('#status').fadeOut(); // will first fade out the loading animation
 			$('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+			$('#status').fadeOut(); // will first fade out the loading animation
 			$('body').delay(350).css({'overflow':'visible'});
 		})
 	//]]>
